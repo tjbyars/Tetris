7e84b6ce185d9061b10fb453eb251cbea3678188
T J Byars <tombyars1@gmail.com>
2026-05-28 15:29:39 +0100

    UI fixes

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -20,5 +20,19 @@ namespace Tetris
         {
             InitializeComponent();
         }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void GameCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlayAgain_Click(object sender, RoutedEventArgs e)       {
+
+        }
     }
 }
